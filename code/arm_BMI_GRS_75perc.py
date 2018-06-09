8dc0f0c0c66932a098226d28bd29d43ca3ae9a3b
Casey Finnicum <casey.finnicum@coyotes.usd.edu>
2018-06-09 01:20:08 -0500

    added code and tried to explicitly comment each step in analysis

--- code/arm_BMI_GRS_75perc.py
+++ code/arm_BMI_GRS_75perc.py
@@ -2,24 +2,25 @@ import pandas as pd
 import Orange
 from orangecontrib.associate.fpgrowth import *
 
+# Read in my relative abundance table which is an m x n datframe (samples are rows, OTUs are columns output from
+# the program mothur, values are trelavtive abunce values
+df = pd.read_csv('../data/test.relabund', sep='\t', index_col='Group')
 
-# read in my relative abundance table which is an m x n datframe (samples are rows, OTUs are columns output from Mothur)
-df = pd.read_csv('../data/BMI_GRS_subsampled_75.opti_mcc.relabund', sep='\t', index_col='Group')
-
-# drop sonme unwanted columns from the datafram
+# drop sonme unwanted columns from the dataframe
 df = df.iloc[:, 2:]
 
 # if the relative abundance is above 0.1, indicate presence with 1
 df1 = df.where(df <= 0.001, 1)
 # if the relative abundance is below 0.1, indicate presence with 0
 df1 = df1.where(df1 >= 0.001, 0).astype(int)
-df2 = df1.loc[:, df1.var() != 0.0]
+# remove OTUs that have 0 variance
+df2 = df1.loc[:, df1.var() != 0]
 
 # write out the processed data that has been converted, this can be used in the follwoing steps as well as uploaded
 # into the Orange GUI to test out and compare
 df2.to_csv('../data/BMI_GRS_0.001_75.tab', sep='\t', index=False)
 
-## Steps below are very similar to the documentation provide from Orange
+# Steps below are very similar to the documentation provide from Orange
 
 # Read in the tab seperated file created in the previous step as Orange.data.table
 table = Orange.data.Table('../data/BMI_GRS_0.001_75.tab')
@@ -40,8 +41,7 @@ names = {item: '{}={}'.format(var.name, val)
 # write out the results of the rules mined as a text file
 with open('../data/results.txt', 'w') as f:
     for ante, cons, supp, conf in rules[:]:
-        supp = supp*2
+        supp = supp * 2
         print(', '.join(names[i] for i in ante), '-->',
               names[next(iter(cons))],
               '(supp: {}, conf: {})'.format(supp, conf), file=f)
-
